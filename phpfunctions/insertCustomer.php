5c37dc7c65d8e1f27dd5b1c89fbb29699ba6917f
Ko-Chen Chen <jack141094@gmail.com>
2017-04-19 19:27:58 -0400

    finished register webpage

--- phpfunctions/insertCustomer.php
+++ phpfunctions/insertCustomer.php
@@ -17,12 +17,29 @@
 		$email = $_POST['email'];
 		$password = $_POST['pass'];
 		$name = $_POST['name'];
-		$phone = $_POST['mobile'];
 		$income = $_POST['income'];
 		$age = $_POST['age'];
 		$gender = $_POST['gender'];
-		
-		$query = ("INSERT INTO customer (Email, Password, Name, Phone, Income, Age, Gender) VALUES ('$email', '$password', '$name', '$phone', '$income', '$age', '$gender')");
+		$address = $_POST['address'];
+		$phone = $_POST['mobile'];
+		$revenue = $_POST['revenue'];
+
+		if (isset($_POST['Buyer']))
+		{
+			$query = ("INSERT INTO customer (Email, Password, Name, Income, Age, Gender) VALUES ('$email', '$password', '$name', '$income', '$age', '$gender')");
+		}
+		else if (isset($_POST['Seller']))
+		{
+			if (isset($_POST['indi']))
+			{
+				$query = ("INSERT INTO seller (Email, Password, Name, Address, Phone, Revenue, Type) VALUES ('$email', '$password', '$name', '$address', '$phone', '$revenue', '0')");
+			}
+			else if (isset($_POST['supplier']))
+			{
+				$query = ("INSERT INTO seller (Email, Password, Name, Address, Phone, Revenue, Type) VALUES ('$email', '$password', '$name', '$address', '$phone', '$revenue', '1')");
+			}
+			
+		}
 
 		if(mysqli_query($conn, $query))
 		{
