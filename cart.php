e09aa9e2f1a12b86e87049dcd8332bda7b71a5c9
Ko-Chen Chen <jack141094@gmail.com>
2017-04-27 14:52:46 -0400

    delete character

--- cart.php
+++ cart.php
@@ -3,7 +3,6 @@ session_start();
 $ids = $_GET["item"];
 if(empty($_SESSION["gwc"]))
 {
- //1.购物车是空的，第一次点击添加购物车
  $arr = array(
  array($ids,1)
  );
@@ -11,9 +10,7 @@ if(empty($_SESSION["gwc"]))
 }
 else
 {
- //不是第一次点击
- //判断购物车中是否存在该商品
- $arr = $_SESSION["gwc"]; //先存一下
+ $arr = $_SESSION["gwc"];
  $chuxian = false;
  foreach($arr as $v)
  {
@@ -24,7 +21,6 @@ else
  }
  if($chuxian)
  {
- //3.如果购物车中有该商品
  for($i=0;$i<count($arr);$i++)
  {
   if($arr[$i][0]==$ids)
@@ -36,7 +32,6 @@ else
  }
  else
  {
- //2.如果购物车中没有该商品
  $asg = array($ids,1);
  $arr[] = $asg;
  $_SESSION["gwc"] = $arr;
@@ -44,12 +39,8 @@ else
 }
 ?>
 
-<h2>购物车中有以下商品：</h2>
 <table cellpadding="0" cellspacing="0" border="1" width="100%">
  <tr>
- <td>商品名称</td>
- <td>商品单价</td>
- <td>购买数量</td>
  <td></td>
  </tr>
  <?php
@@ -72,12 +63,9 @@ else
  <td>{$n[1]}</td>
  <td>{$n[2]}</td>
  <td>{$v[1]}</td>
- <td><a href='shanchu.php?ids={$v[0]}'>删除</a></td>
  </tr>";}
  }
  ?>
 </table>
 <div>
-<a href="gouwuche.php" rel="external nofollow" rel="external nofollow" >查看购物车</a> 
-<a href="main.php" rel="external nofollow" rel="external nofollow" >浏览商品</a> 
-<a href="zhanghu.php" rel="external nofollow" rel="external nofollow" >查看账户</a> </div> 14 15 </body>
+</body>
